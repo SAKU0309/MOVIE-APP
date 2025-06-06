f391d1cbb6b1dc0d3a6332587268783fd10cd06a
Sakshi <sakshichy14@gmail.com>
2025-06-06 22:03:50 +0530

    Final

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY="9664f562f0d07a06813cf61abee5c223";
+const BASE_URL="https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results
+};
